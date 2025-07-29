753d4aa648393b6841337808227c4f6a6d9f5edd
Huy Pham <p.q.huy97@gmail.com>
2025-07-29 15:27:05 +0700

    use external

--- src/StructHash.sol
+++ src/StructHash.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 pragma abicoder v2;
 
 library StructHash {
-  function _hash(bytes memory abiEncodedUserOrder) internal pure returns (bytes32) {
+  function _hash(bytes memory abiEncodedUserOrder) external pure returns (bytes32) {
     return _hash(abi.decode(abiEncodedUserOrder, (Order)));
   }
 
