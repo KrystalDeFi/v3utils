4d5728f8ab60f269ab8f7d68b31015c95bb2056f
Nguyen Khoi Nguyen <nguyennk92@gmail.com>
2026-02-05 09:21:21 +0700

    update grant role to revoke personal deployer admin

--- script/GrantRole.s.sol
+++ script/GrantRole.s.sol
@@ -17,6 +17,9 @@ interface IV3Initializer {
         address[] calldata nfpms
     ) external;
     function grantRole(bytes32 role, address account) external;
+    function revokeRole(bytes32 role, address account) external;
+
+    function DEFAULT_ADMIN_ROLE() external view returns (bytes32);
     function OPERATOR_ROLE() external view returns (bytes32);
     function ADMIN_ROLE() external view returns (bytes32);
 }
@@ -24,14 +27,46 @@ interface IV3Initializer {
 contract V3AutomationGrantRoleScript is CommonScript {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployerAddress = vm.addr(deployerPrivateKey);
         address deploymentAddress = getV3AutomationDeploymentAddress();
-        // 0x84C4c905Fc18313f0A8e20855a067b7caaca5922;
+        address multisig = vm.envAddress("MULTISIG");
+        console.log("mutltisig :", multisig);
+        console.log("deploymentAddress :", deploymentAddress);
+        console.log("deployerAddress :", deployerAddress);
 
+        // 0x84C4c905Fc18313f0A8e20855a067b7caaca5922;
         vm.startBroadcast(deployerPrivateKey);
         IV3Initializer v3automation = IV3Initializer(deploymentAddress);
         v3automation.grantRole(v3automation.OPERATOR_ROLE(), 0x4b82847C82087ea19418beD079966810f64f39f4);
         v3automation.grantRole(v3automation.OPERATOR_ROLE(), 0x9729585607568Dcbf1f5f3802AfBf5B221702932);
-        v3automation.grantRole(v3automation.ADMIN_ROLE(), 0x9A99252A76f7B40Cbf2002a8dB2977C85fA306DF);
+        v3automation.grantRole(v3automation.DEFAULT_ADMIN_ROLE(), multisig);
+        v3automation.grantRole(v3automation.ADMIN_ROLE(), multisig);
+        v3automation.revokeRole(v3automation.ADMIN_ROLE(), deployerAddress);
+        v3automation.revokeRole(v3automation.DEFAULT_ADMIN_ROLE(), deployerAddress);
+
+        vm.stopBroadcast();
+    }
+
+    function test() external {}
+}
+
+contract V3UtilsGrantRoleScript is CommonScript {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployerAddress = vm.addr(deployerPrivateKey);
+        address deploymentAddress = getV3UtilsDeploymentAddress();
+        address multisig = vm.envAddress("MULTISIG");
+        console.log("mutltisig :", multisig);
+        console.log("deploymentAddress :", deploymentAddress);
+        console.log("deployerAddress :", deployerAddress);
+
+        vm.startBroadcast(deployerPrivateKey);
+        // 0x84C4c905Fc18313f0A8e20855a067b7caaca5922;
+        IV3Initializer v3utils = IV3Initializer(deploymentAddress);
+        v3utils.grantRole(v3utils.DEFAULT_ADMIN_ROLE(), multisig);
+        v3utils.grantRole(v3utils.ADMIN_ROLE(), multisig);
+        v3utils.revokeRole(v3utils.ADMIN_ROLE(), deployerAddress);
+        v3utils.revokeRole(v3utils.DEFAULT_ADMIN_ROLE(), deployerAddress);
 
         vm.stopBroadcast();
     }
