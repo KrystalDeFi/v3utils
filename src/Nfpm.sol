2fe753b9f81845127dd28a057e55eee611461699
Nguyen Khoi Nguyen <nguyennk92@gmail.com>
2026-07-16 10:41:10 +0700

    revert and ignore Nfpm and StructHash from forge fmt

--- src/Nfpm.sol
+++ src/Nfpm.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import {
-    INonfungiblePositionManager as IUniV3NonfungiblePositionManager
-} from "v3-periphery/interfaces/INonfungiblePositionManager.sol";
+import {INonfungiblePositionManager as IUniV3NonfungiblePositionManager} from
+    "v3-periphery/interfaces/INonfungiblePositionManager.sol";
 
 interface INonfungiblePositionManager is IUniV3NonfungiblePositionManager {
     /// @notice mintParams for algebra v1
